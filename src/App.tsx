eb68552afac01189b99bfb771013af95d9b690c3
jordanbastin <jord.bastin@gmail.com>
2023-03-15 11:44:25 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="mx-auto flex h-screen w-screen max-w-7xl flex-col px-4 sm:px-6 lg:px-8">
+      <div className="mx-auto mt-5 flex w-full max-w-3xl flex-col">
+        <h1 className={'text-red-500'}>Hello World</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
